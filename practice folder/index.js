cd81f01e6d4958c73f8941c0967e270d265cf2d7
Martin Odongo <martinwere14@gmail.com>
2024-10-10 11:46:52 +0300

    learnt object destructurintg  in js

--- practice folder/index.js
+++ practice folder/index.js
@@ -127,13 +127,117 @@
 // }
 //     }
 
-async function fetchData(){
-    const response = await fetch("https://api.example.com/data")
-    const data = await response.json();
-    return data;
-}
-fetchData().then(data=>{
-    console.log(data);
-}).catch(error=>{
-    console.error(error);
-});
+// async function fetchData(){
+//     const response = await fetch("https://api.example.com/data")
+//     const data = await response.json();
+//     return data;
+// }
+// fetchData().then(data=>{
+//     console.log(data);
+// }).catch(error=>{
+//     console.error(error);
+// });
+
+// building AudioProcessingEvent
+
+// fetch('https://api.example.com/data',{
+//     method:'POST',
+//     headers:{
+//         'Authorization':'Bearer <your_token>',
+//         'content-Type':'application/json'
+//     },
+//     body: JSON.stringify({
+//         name:'New Data',
+//         description:'Important information'
+//     })//it should changed to a string
+// })
+// .then(response=>{
+//     if(!response.ok){
+//         throw new Error('Network response was not ok')
+//     }
+//     return response.json();
+// })
+// .then(data=>{
+//     console.log('Success:',data);
+// })
+// .catch(error=>{
+//     console.error('Error:',error);
+// })
+// //the above code in async/await .
+// async function fetchData(){
+//     try{
+//         const response = await fetch('https://api.example.com/data')
+//         if(!response.ok){
+//             throw new Error('Network response was not ok');
+//         }
+//         const data = await response.json();
+//         console.log('Success:',data);   
+//     } catch(error){
+//         console.error('Error;',error);
+//     }
+// }
+// fetchData();
+
+// 
+// const myPromise = new promise((resolve, reject)=>{
+//     const error = false;
+//     if (error){
+//         resolve("Yes ,resolved the promise");
+//     }else{
+//         reject("No rejected the promise");
+//     }
+// });
+// console.log(myPromise)
+// myPromise.then(value =>{
+//     console.log(value)
+// })
+// const myPromise = new Promise((resolve, reject)=>{
+//      const error = false;
+//      if(error){
+//         reject("No, rejected the promise");
+//      }else{
+//         resolve("Yes, resolved the promise")
+//      }
+// });
+// console.log(myPromise);
+
+// myPromise.then(value=>{
+//     console.log(value);
+// }) .catch(error=>{
+//     console.log(error);
+// })
+// .then(value =>{
+//     return value +1;
+// })
+// .then(newValue=>{
+//     console.log(newValue);
+// })
+// .catch(error=>{
+//     console.log(error)
+// })
+// // reason we have promises is requesting data from another server in the web.makes sense when we work with thenables.fetch doesn't require us to create a new promise.
+// //
+// const mySecondPromise = new Promise((resolve, reject)=>{
+//     setTimeout(function(){
+//         resolve("Your promise is resolved")
+//     },2000)
+// })
+// mySecondPromise.then(value=>{
+// console.log(value);
+// })
+// myPromise.then(value=>{
+//     console.log(value);
+// })
+
+// fetch will request data from another place on the web for example
+
+const users = fetch("https://JSONplaceholder.typicode.com/users")
+//pending state of the promise
+console.log(users);
+fetch("https://JSONplaceholder.typicode.com/users")
+    .then(response=>{
+        return response.json();
+    })
+    then(data=>{
+        console.log()
+    })
